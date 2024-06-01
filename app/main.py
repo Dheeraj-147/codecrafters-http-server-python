f7a1d70ca2802a6047d9325bad181b55079a55c7
Dheeraj-147 <105336260+Dheeraj-147@users.noreply.github.com>
2024-06-01 22:50:51 +0530

    pass5

--- app/main.py
+++ app/main.py
@@ -10,7 +10,12 @@ def main():
     
     server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
     client_socket,address=server_socket.accept() # wait for client
-    client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
-
+    request = client_socket.recv(1024).decode("utf-8")
+    request=request.split(" ")[0]
+    client_socket.sendall(request)
+    # if address[0] != "":
+    #     client_socket.sendall(b"HTTP/1.1 200 OK\r\n\r\n")
+    # else:
+    #     client_socket.sendall(b"HTTP/1.1 404 Not Found\r\n\r\n")
 if __name__ == "__main__":
     main()
