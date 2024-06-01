f4164621abea343e01e757a8fd92575bb59fe152
Dheeraj-147 <105336260+Dheeraj-147@users.noreply.github.com>
2024-06-02 00:05:56 +0530

    passs001

--- app/main.py
+++ app/main.py
@@ -13,8 +13,8 @@ def main():
     request = client_socket.recv(1024).decode("utf-8")
     request_line=request.split("\n")[0]
     req=request_line.split(" ")
-    req1=req[2].split("User-Agent: ")[1].split("\r")[0]
-    st="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(len(req1))+"\r\n\r\n"+req1
+    #req1=req[2].split("User-Agent: ")[1].split("\r")[0]
+    st="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: "+str(len(req))+"\r\n\r\n"+req[2]
     # a=req[1].split("/")
     # leng=""
     # if len(a)==2 and req[1]!="/":
